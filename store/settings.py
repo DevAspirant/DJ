cafb2411fd6547c50dc7973a537a623991fd533d
ammar <devAspirant@gmail.com>
2020-07-08 14:11:18 -0400

    test

--- store/settings.py
+++ store/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2$g9@zuix9_@c2#yk2k#wpyqum!)ym+xb$hdcii(c@+hipx95q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1']
 
 
 # Application definition
