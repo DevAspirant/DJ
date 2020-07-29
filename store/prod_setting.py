93a96784cd131ff23547fddfd7063c9523469c10
ammar <devAspirant@gmail.com>
2020-07-29 12:06:29 -0400

    db config

--- store/prod_setting.py
+++ store/prod_setting.py
@@ -2,6 +2,8 @@ import os
 
 from .settings import *
 
+import dj_database_url
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
@@ -11,3 +13,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['https://marvelous-everglades-50690.herokuapp.com/']
 
+DATABASES = {
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+}
